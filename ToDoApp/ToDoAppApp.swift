6e3d6153d0444f974d889bc65112268ec1c54e4c
noH4uK90 <noh4uk90@gmail.com>
2024-06-17 11:59:08 +0300

    Initial Commit

---
+++ ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Иван Спирин on 6/17/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
